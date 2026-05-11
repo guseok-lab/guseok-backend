ad3d1e5302c93f3acc0aedc8c4bc6bb023001f83
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2026-05-11 09:30:51 +0900

    feat: WebSocket 설정 클래스 파일 생성

---
+++ src/main/java/com/guseok/guseokbackend/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.guseok.guseokbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final DroneStreamHandler droneStreamHandler;
+
+    public WebSocketConfig(DroneStreamHandler droneStreamHandler) {
+        this.droneStreamHandler = droneStreamHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(droneStreamHandler, "/ws/drone")
+                .setAllowedOrigins("*");  // 개발 중엔 전체 허용
+    }
+}
