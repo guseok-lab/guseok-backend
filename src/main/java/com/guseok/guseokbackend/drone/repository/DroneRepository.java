31a29576358829afe7db816eb4614bf23a5aabbd
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2026-05-11 11:07:16 +0900

    feat: DB 접근 담당 DroneRepository.java 생성

---
+++ src/main/java/com/guseok/guseokbackend/drone/repository/DroneRepository.java
@@ -0,0 +1,12 @@
+package com.guseok.guseokbackend.drone.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guseok.guseokbackend.drone.entity.Drone;
+import com.guseok.guseokbackend.drone.enums.DroneConnectionStatus;
+
+public interface DroneRepository extends JpaRepository<Drone, Long> {
+    Optional<Drone> findByStatus(DroneConnectionStatus status);
+}
